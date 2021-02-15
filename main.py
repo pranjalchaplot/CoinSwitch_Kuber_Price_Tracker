1254584b5d0c2e4281988304b095ddabcd0d77b6
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-02-15 13:23:22 +0530

    initial setup

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("helloworld")
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import json
+import time
+
+csk = requests.get('https://coinswitch.co/')
+soup = BeautifulSoup(csk.content, 'html.parser')
